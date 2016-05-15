59a25e38723a1886da6ebd84e84700d283f29925
minamhere <minamhere@users.noreply.github.com>
2016-05-14 22:15:00 -0400

    Create fare class dict

--- wnfc.user.js
+++ wnfc.user.js
@@ -12,19 +12,20 @@
 
 (function() {
 
-var fc_T = 72;
-var fc_N = 72;
-var fc_M = 72;
-var fc_S = 72;
-var fc_O = 72;
-var fc_R = 74;
-var fc_W = 76;
-var fc_H = 76;
-var fc_Q = 78;
-var fc_B = 78;
-var fc_L = 80;
-var fc_YL = 100;
-var fc_K = 120;
+var fare_classes = {
+    {'T':72},
+    {'N':72},
+    {'M':72},
+    {'S':72},
+    {'O':72},
+    {'R':74},
+    {'W':76},
+    {'H':76},
+    {'Q':78},
+    {'B':78},
+    {'L':80},
+    {'YL':100},
+    {'K':120}};
 
     $('[class*=boundRadio]').each(function() {
         var re = /(\d+\.*\d*)\@(\w)\@.*@/;
